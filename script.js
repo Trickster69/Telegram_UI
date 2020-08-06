ce2905f27a5d1f332bd2ed231f06830e6480cb88
Sergei <demchuk.1993@gmail.com>
2020-08-06 22:52:29 +0300

    contacts bar

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let menu = document.querySelector(".header_tg");
+
+// menu.onmouseenter = function () {
+//     alert("click")
+// };
+
+menu.addEventListener("mouseenter", () => {
+    alert("click");
+});
