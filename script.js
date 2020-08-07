7c816ef2cc2ca19f3eb8b458c21ae15370a21e5d
Sergei <demchuk.1993@gmail.com>
2020-08-07 03:30:20 +0300

    contact scrollbar

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 'use strict';
 
 let menu = document.querySelector(".header_tg");
-
+let bar = document.querySelector(".icon_menu");
 // menu.onmouseenter = function () {
 //     alert("click")
 // };
 
-menu.addEventListener("mouseenter", () => {
-    alert("click");
+bar.addEventListener("mouseenter", () => {
+
+    // bar.style.width ///
+
+
+
 });
